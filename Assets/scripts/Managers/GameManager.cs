6734fc9b1501ce2023ff939bc8b7587009152115
Agus Ruscio <agustin.ruscio6@gmail.com>
2023-01-20 12:47:57 -0300

    Build error Fixed

--- Assets/scripts/Managers/GameManager.cs
+++ Assets/scripts/Managers/GameManager.cs
@@ -5,7 +5,6 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
